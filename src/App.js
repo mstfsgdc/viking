69fe21fffd127fbb5893196722161b30ede324bd
Mustafa Sagdic <mmsagdic@gmail.com>
2022-07-21 14:39:27 +0300

    add other components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { Header } from './components';
+import { Header, Main, Footer } from './components';
 
 function App() { // TODO ADD license
   return (
     <div className="w-screen min-h-screen bg-background-50 font-poppins">
-      <div className="container mx-auto">
+      <div className="container flex flex-col justify-between h-screen mx-auto">
         <Header />
+        <Main />
+        <Footer />
       </div>
     </div>
   );
